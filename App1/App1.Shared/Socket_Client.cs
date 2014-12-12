2e8eaaa188903834803376416be67245ad6119c8
Robert <rslepecki@mobitouch.net>
2014-12-12 19:56:25 +0100

    init

---
+++ App1/App1.Shared/Socket_Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;
+using Windows.Networking.Sockets;
+
+namespace App1
+{
+
+    class Socket_Client
+    {
+        DatagramSocket Client;
+
+        public void Init()
+        {
+            Client = new DatagramSocket();
+            Client.
+        }
+        
+    }
+}
